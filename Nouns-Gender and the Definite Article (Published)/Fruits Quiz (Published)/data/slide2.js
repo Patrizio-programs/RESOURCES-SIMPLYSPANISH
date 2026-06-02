a2ee7d463d8505193da15225da592284bfe31a49
PatrizioTheDev <patickmedley@gmail.com>
2026-06-02 16:26:39 -0400

    ggs

---
+++ Nouns-Gender and the Definite Article (Published)/Fruits Quiz (Published)/data/slide2.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{C7954250-A740-49DB-BC0C-7CB8ADB8649E}'];loadHandler&&loadHandler(1, '<div id="spr0_11ef20"><div id="spr1_11ef20" class="kern slide"><img id="img0_11ef20" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_11ef20" class="kern slide"><div id="wo_1_0"></div></div></div>', '{"s":[]}');})();
